c05ac605c0319ad1ddbd5336dce706133b91ac08
nwatabou <wataru1.11@gmail.com>
2020-02-09 19:11:58 +0900

    Initial Commit

---
+++ testGifAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGifAnimation
+//
+//  Created by nancy on 2020/02/09.
+//  Copyright © 2020 nancy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
